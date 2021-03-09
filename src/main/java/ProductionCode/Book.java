911d927b2bacc0cc27251155545b13268c435b0d
RashaSalahat <rasha.salahat2000@gmail.com>
2021-03-09 13:17:15 +0200

    First

---
+++ src/main/java/ProductionCode/Book.java
@@ -0,0 +1,63 @@
+package ProductionCode;
+
+public class Book {
+private String bookTitle;
+private String bookAuthor;
+private String bookSignature;
+private String bookISBN_10;
+private String bookState;
+//set and get methods  bookTitle
+public void setBookTitle(String bookTitle_value)
+{
+	bookTitle=bookTitle_value;
+}
+public String getBookTitle()
+{
+	return bookTitle;
+}
+
+//set and get methods bookAuthor
+public void setBookAuthor(String bookAuthor_value)
+{
+	bookAuthor=bookAuthor_value;
+}
+public String getBookAuthor()
+{
+	return bookAuthor;
+}
+
+//set and get methods bookSignature 
+public void setBookSignature(String bookSignature_value)
+{
+	bookSignature=bookSignature_value;
+}
+public String getBookSignature()
+{
+	return bookSignature;
+}
+
+//set and get methods bookISBN_10
+public void setBookISBN_10(String bookISBN_10_value)
+{
+	bookISBN_10=bookISBN_10_value;
+}
+public String getBookISBN_10()
+{
+	return bookISBN_10;
+}
+
+//set and get methods bookState
+
+public void setBookState(String bookState_value)
+{
+	bookState=bookState_value;
+}
+public String getBookState()
+{
+	return bookState;
+}
+
+
+
+
+}
